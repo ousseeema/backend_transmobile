d05d39b050d51c05583ca5c7f8ec98b4624a8a29
ousseeema <oussama.ferchichi01@gmail.com>
2024-02-22 22:30:21 +0100

    trip model

---
+++ model/tripModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+
+const tripModel = mongoose.Schema({
+
+  transporter :mongoose.Schema.Types.ObjectId,
+  City :{
+    type : [{
+      type : Map , 
+      of : mongoose.Schema.Types.Mixed
+    }],
+    required : true ,
+    minlength : 2,
+  },
+  Home_pick_up :{
+    type : Boolean,
+    default : false,  
+
+  },
+  Home_delivery :{
+    type : Boolean,
+    default : false,
+
+  },
+  packages :{
+    type : [{
+      type :Map,
+      of :  mongoose.Schema.Types.Mixed
+    }],
+    default : [],
+    required : false,
+    select: false
+  },
+  isDone :{
+    type: Boolean,
+    default : false,
+  }
+
+});
+
+
+module.exports = mongoose.model('tripModel', tripModel);

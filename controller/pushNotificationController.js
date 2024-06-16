5c3fc936f3aab1493869dd0e1cf47be214adcf1b
ousseeema <oussama.ferchichi01@gmail.com>
2024-06-16 23:47:10 +0100

    push notification controller

---
+++ controller/pushNotificationController.js
@@ -0,0 +1,66 @@
+const PushNotificationService = require("../utils/sendNotification.service");
+const {ONE_SIGNAL_CONFIG} =require('../config/config.app');
+
+// send notification for all users
+exports.SendNotification = (req, res, next) => {
+ var message = {
+  app_id: ONE_SIGNAL_CONFIG.APP_ID,
+  contents:{
+    'en':"Test Push Notifications"
+  },
+  included_segments:["All"], 
+  content_available:true,
+  small_icon: "ic_notification_icon",
+  data:{
+    PushTitle : "Notifications",
+  }
+
+
+
+ }
+ PushNotificationService.SendNotification(message, (err, result) => {
+  if(err){
+    return next(err);
+  }
+  return res.status(200).send(
+    {
+      message: 'success',
+      data: result
+    }
+  )
+ })
+
+};
+
+
+// push notification for single user a plus 
+exports.sendNotifcationToDevice = (req, res, next) => {
+  var message = {
+   app_id: ONE_SIGNAL_CONFIG.APP_ID,
+   contents:{
+     'en':"Test Push Notifications"
+   },
+   included_segments:["included_player_ids"], 
+   included_playerè_ids : req.body.devices ,
+   content_available:true,
+   small_icon: "ic_notification_icon",
+   data:{
+     PushTitle : "Notifications",
+   }
+ 
+ 
+ 
+  }
+  PushNotificationService.SendNotification(message, (err, result) => {
+   if(err){
+     return next(err);
+   }
+   return res.status(200).send(
+     {
+       message: 'success',
+       data: result
+     }
+   )
+  })
+ 
+ };
--- utils/sendNotification.service.js
+++ utils/sendNotification.service.js
@@ -1,6 +1,7 @@
+const { json } = require('express');
 const{ONE_SIGNAL_CONFIG}= require('../config/config.app');
 async function SendNotification(data, callback) {
-  var header = {
+  var headers = {
     "Content-Type" : "application/json; charset=utf-8" ,
     "Authorization" :"basic "+ONE_SIGNAL_CONFIG.API_KEY,
 
@@ -10,9 +11,27 @@ async function SendNotification(data, callback) {
   var options ={
     host :"onesignal.com",
     port: 443,
-    
-    
+    path:"/api/v0/notifications",
+     method: "POST",
+     headers: headers
   }
+  var http = require('http');
+  var req =  http.request(options, function(res){
+   res.on('data', function(data){
+    console.log(JSON.parse(data));
+    return callback(null , JSON.parse(data));
+   });
+  });
+  req.on("error", function(err){
+  return  callback({message:err.message});
+
+  });
+  req.write(json.stringify(data));
+  req.end();
+  
   
 
+}
+module.exports = {
+  SendNotification
 }
